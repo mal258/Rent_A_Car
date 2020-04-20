ff2d93cde7d6223fae90100d2c383b4e0e4da115
machine@258 <machinelearning@258>
2020-04-20 02:56:31 -0700

    changed

--- system/models.py
+++ system/models.py
@@ -1,19 +1,50 @@
 from django.db import models
+
+#from multiselectfield import MultiSelectField
 from django import forms
 
 def uploaded_location(instance, filename):
     return ("%s/%s") %(instance.car_name,filename)
 
+CAR_TYPE = (
+        ('small car', 'SMALL CAR'),
+        ('full-size_car', 'FULL-SIZE CAR'),
+        ('truck', 'TRUCK'),
+        ('luxury', 'LUXURY')
+        )
+# CHARGE = [
+#      ('1-5hours'),
+#      ('6-10hours')
+#      ]
+# CONDITION = (
+#     ('Good'),
+#     ('Needs Cleaning'),
+#     ('Needs Maintenance')
+# )
+
 class Car(models.Model):
-    image = models.ImageField(upload_to=uploaded_location,null=True, blank=True, width_field="width_field", height_field="height_field")
-    height_field = models.IntegerField(default=0)
-    width_field = models.IntegerField(default=0)
+
+    image = models.ImageField(upload_to=uploaded_location,null=True, blank=True)
     car_name = models.CharField(max_length=100)
-    company_name = models.CharField(max_length=100)
+    car_type = models.CharField(max_length=50, choices=CAR_TYPE)
+    model = models.IntegerField()
     num_of_seats = models.IntegerField()
-    cost_par_day = models.CharField(max_length=50)
-    content = models.TextField()
+    reg_tag = models.CharField(max_length=100)
+    cur_milage = models.IntegerField()
+    last_serv = models.IntegerField()
+    cost_per_day = models.CharField(max_length=50)
+    rent_loc = models.IntegerField()
     like = models.IntegerField(default=0)
+    class vehicle(models.TextChoices):
+        Good = 'Good'
+        Need_cleaning = 'Need Cleaning'
+        Need_maintenance = 'Needs Maintenance'
+    vehicle_cond = models.CharField(max_length=100, choices=vehicle.choices)
+    # class cost(models.TextChoices):
+    #     first = '1-5hours'
+    #     second = '6-10hours'
+    # cost = models.CharField(max_length=50, choices=cost.choices)
+
 
     def __str__(self):
         return self.car_name
@@ -43,6 +74,8 @@ class PrivateMsg(models.Model):
 #shreyus
 class Location(models.Model):
     loc_name = models.CharField(max_length=200)
+    loc_zip = models.IntegerField()
+    loc_id = models.IntegerField()
     address = models.TextField()
     vehicle_cap = models.IntegerField(default=0)
 
